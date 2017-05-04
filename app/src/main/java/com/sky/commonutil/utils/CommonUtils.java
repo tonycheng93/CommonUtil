2e9a9d556fb7255d01f136b4cc5a797ccb8c08c5
baocheng <baocheng@skyworth.com>
2017-05-04 18:07:41 +0800

    add some utils

---
+++ app/src/main/java/com/sky/commonutil/utils/CommonUtils.java
@@ -0,0 +1,29 @@
+package com.sky.commonutil.utils;
+
+import android.os.Environment;
+
+/**
+ * Created by tonycheng on 2017/5/4.
+ */
+
+public class CommonUtils {
+
+    /* Checks if external storage is available for read and write */
+    public static boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+
+    /* Checks if external storage is available to at last read */
+    public static boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+}
